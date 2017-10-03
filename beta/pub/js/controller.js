7cd306831085f78c62394e07343b1209f968726b
Jek Hales <halesyy@gmail.com>
2017-10-03 14:17:48 +1100

    beta introduced to master branch

---
+++ beta/pub/js/controller.js
@@ -0,0 +1,8 @@
+/*
+ * The most beautiful JS file, controlls everything on
+ * startup so it's all presented beautifully.
+ */
+
+  $(window).on('load resize', function(){
+    // $('#content').height( $(window).height() - $('#nav').outerHeight(true) );
+  });
